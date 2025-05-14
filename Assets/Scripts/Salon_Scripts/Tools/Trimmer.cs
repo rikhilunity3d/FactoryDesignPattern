6cbb1875f30f31d6d4a83382b0d51bdc2f956086
sonamjain2810 <rikhil.unity3d@gmail.com>
2025-05-14 20:28:28 +0530

    Revert "Merge branch 'feature/SoundManager' into leg-stage-flow"
    This reverts commit 474e44e604298e0f3a43eb79c80ee16c9aaded03, reversing
    changes made to 8e69711779ea537df4615c857cae2f5f06bee0ce.

--- Assets/Scripts/Salon_Scripts/Tools/Trimmer.cs
+++ Assets/Scripts/Salon_Scripts/Tools/Trimmer.cs
@@ -1,16 +1,9 @@
-using System.Runtime.CompilerServices;
-using Obvious.Soap;
 using UnityEngine;
 
-[RequireComponent(typeof(AudioClip))]
 public class Trimmer : MonoBehaviour, IBeautyTool
 {
     [SerializeField] private LegStageType requiredStage = LegStageType.Hairy;
     [SerializeField] private LegCareManager legCareManager;
-    [SerializeField] private AudioClip trimmerSound;
-    [SerializeField] private SoundEventChannelSO soundEventChannel;
-
-
 
     public void UseTool(GameObject target)
     {
@@ -18,7 +11,6 @@ public class Trimmer : MonoBehaviour, IBeautyTool
         {
             Debug.Log("Trimmer is used on " + target.name);
             Debug.Log("Trimmer used correctly!");
-            soundEventChannel.RaiseEvent(trimmerSound);
             legCareManager.AdvanceStage();
         }
         else
